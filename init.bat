3b853dda6b48ee23c152f4621d0948b81ff4671f
Dominic Hemken <dphemken@gmail.com>
2024-02-10 09:34:56 -0600

    updated scripts

--- init.bat
+++ init.bat
@@ -1,4 +1,3 @@
-Echo starting Script
-curl https://raw.githubusercontent.com/DHemken97/GAH_Silent_Install/main/install.bat >> install.bat
-start install.bat
+start cmd /c "curl https://raw.githubusercontent.com/DHemken97/GAH_Silent_Install/main/install.bat >> install.bat &&
+start install.bat && exit 0"
 exit
